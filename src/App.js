71c3ea4ef0c4a6102cda7f02e67e915064f5e5c7
Rohan Kalsi <rkoalsi2000@gmail.com>
2020-02-25 13:33:37 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button } from "antd";
+import List from "./Itemlist";
+import "antd/dist/antd.css";
+import { Layout, Row, Col } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Content } = Layout;
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      items: []
+    };
+  }
+
+  onChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      term: "",
+      items: [...this.state.items, this.state.term]
+    });
+  };
+
+  render() {
+    return (
+      <div className="main">
+        <div className="App">
+          <Layout>
+            <Content style={{ height: "100vh" }}>
+              <Row style={{ height: "100%" }}>
+                <Col
+                  style={{
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center"
+                  }}
+                  span={12}
+                >
+                  <form onSubmit={this.onSubmit}>
+                    <input
+                      placeholder="Add Things to do"
+                      value={this.state.term}
+                      onChange={this.onChange}
+                    />
+                    <Button htmlType="submit" type="danger">
+                      Submit
+                    </Button>
+                  </form>
+                </Col>
+                <Col
+                  style={{
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center"
+                  }}
+                  span={12}
+                >
+                  <div className="itemList">
+                    <List items={this.state.items} />
+                  </div>
+                </Col>
+              </Row>
+            </Content>
+          </Layout>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
